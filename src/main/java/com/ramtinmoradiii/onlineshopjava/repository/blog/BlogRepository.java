b09cf78822336d7fd35c19e20b85ab39aab45bcb
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-01-07 01:18:25 +0330

    Add BlogRepository for managing blog entities and their status

---
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/repository/blog/BlogRepository.java
@@ -0,0 +1,15 @@
+package com.ramtinmoradiii.onlineshopjava.repository.blog;
+
+import com.ramtinmoradiii.onlineshopjava.entity.blog.Blog;
+import com.ramtinmoradiii.onlineshopjava.entity.blog.enums.BlogStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+    List<Blog> findAllByStatus(BlogStatus status);
+
+    List<Blog> findTopFiveByStatusOrderByPublishDateDesc(BlogStatus status);
+}
