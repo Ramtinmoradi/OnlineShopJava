80093de7fb1a7ea6df55c43fa26d09bc7b831bf9
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-01-07 02:23:50 +0330

    Add Cart and CartItem entity in another package

---
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/entity/cart/CartItem.java
@@ -0,0 +1,36 @@
+package com.ramtinmoradiii.onlineshopjava.entity.cart;
+
+import com.ramtinmoradiii.onlineshopjava.entity.product.ProductVariant;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_variant_id", nullable = false)
+    private ProductVariant productVariant;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Long getRowTotalPrice() {
+        if (productVariant == null) return 0L;
+        long price = (productVariant.getPriceOverride() != null)
+                ? productVariant.getPriceOverride()
+                : productVariant.getProduct().getPrice();
+        return price * quantity;
+    }
+}
