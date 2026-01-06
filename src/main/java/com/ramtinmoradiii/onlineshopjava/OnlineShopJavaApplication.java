3899f8024b45de13ac8122702dcb4cdb443fd702
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-01-06 13:15:54 +0330

    Create Project

---
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/OnlineShopJavaApplication.java
@@ -0,0 +1,11 @@
+package com.ramtinmoradiii.onlineshopjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopJavaApplication.class, args);
+    }
+}
