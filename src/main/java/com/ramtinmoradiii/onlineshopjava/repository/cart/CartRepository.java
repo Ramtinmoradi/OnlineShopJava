c32ad4624d9c1ff13fa6013107e9953f3790c916
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-01-07 02:24:49 +0330

    Add CartRepository and CartItemRepository to another package

---
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/repository/cart/CartRepository.java
@@ -0,0 +1,12 @@
+package com.ramtinmoradiii.onlineshopjava.repository.cart;
+
+import com.ramtinmoradiii.onlineshopjava.entity.cart.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(Long userId);
+}
