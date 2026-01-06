e22456502b651eb262517e4d24c690608719a346
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-01-06 19:25:26 +0330

    Add DataInitializer class to create default roles on application startup

---
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/config/DataInitializer.java
@@ -0,0 +1,32 @@
+package com.ramtinmoradiii.onlineshopjava.config;
+
+import com.ramtinmoradiii.onlineshopjava.entity.user.Role;
+import com.ramtinmoradiii.onlineshopjava.repository.user.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        createRoleIfNotFound("ROLE_USER");
+        createRoleIfNotFound("ROLE_CUSTOMER");
+        createRoleIfNotFound("ROLE_ADMIN");
+    }
+
+    private void createRoleIfNotFound(String name) {
+        if (roleRepository.findByName(name).isEmpty()) {
+            Role role = Role.builder()
+                    .name(name)
+                    .description("نقش سیستمی تولید شده خودکار")
+                    .build();
+            roleRepository.save(role);
+            System.out.println("Role created: " + name);
+        }
+    }
+}
