e4f3d0144636a3dfc737b05e17c5ea06a21f5e95
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-02-13 19:26:55 +0330

    Chore: update ApiResponse class to include errors list and improve success/error method signatures

--- src/main/java/com/ramtinmoradiii/onlineshopjava/dto/common/ApiResponse.java
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/dto/common/ApiResponse.java
@@ -1,38 +1,47 @@
 package com.ramtinmoradiii.onlineshopjava.dto.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
+    private List<ValidationError> errors;
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, "عملیات با موفقیت انجام شد.", data);
+        return new ApiResponse<T>(true, "عملیات با موفقیت انجام شد.", data, null);
     }
 
     public static <T> ApiResponse<T> success() {
-        return new ApiResponse<>(true, "عملیات با موفیت انجام شد.", null);
+        return new ApiResponse<T>(true, "عملیات با موفقیت انجام شد.", null, null);
     }
 
     public static <T> ApiResponse<T> success(T data, String message) {
-        return new ApiResponse<>(true, message, data);
+        return new ApiResponse<T>(true, message, data, null);
     }
 
     public static <T> ApiResponse<T> success(String message) {
-        return new ApiResponse<>(true, message, null);
+        return new ApiResponse<T>(true, message, null, null);
     }
 
     public static <T> ApiResponse<T> error(String message) {
-        return new ApiResponse<>(false, message, null);
+        return new ApiResponse<T>(false, message, null, null);
     }
 
     public static <T> ApiResponse<T> error(String message, T data) {
-        return new ApiResponse<>(false, message, data);
+        return new ApiResponse<T>(false, message, data, null);
+    }
+
+    public static <T> ApiResponse<T> validationError(List<ValidationError> errors) {
+        return new ApiResponse<T>(false, "اطلاعات ورودی نامعتبر است.", null, errors);
     }
 }
