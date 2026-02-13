28c90b872e7a0fca23645fd5560f04a338e9d198
ramtinmoradiii <ramtinmoradiiii@gmail.com>
2026-02-13 19:26:05 +0330

    Chore: replace NotFoundException with ResourceNotFoundException class

--- src/main/java/com/ramtinmoradiii/onlineshopjava/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ramtinmoradiii.onlineshopjava.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/ramtinmoradiii/onlineshopjava/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ramtinmoradiii.onlineshopjava.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
